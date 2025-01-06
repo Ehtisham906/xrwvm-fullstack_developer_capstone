cf33a12e74c62ce5e0a0552a24d6ce5b6798f752
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2025-01-06 15:02:40 +0500

    modified:   server/djangoapp/models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,12 +9,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name  # String representation of the CarMake object
 
 # CarModel model
-class CarModel(models.Model):
+class CarModel(models.Model):  
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     
