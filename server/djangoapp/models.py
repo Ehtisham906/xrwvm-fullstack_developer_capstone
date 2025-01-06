6aa6e5b3b8b5f9b23493ae2f90847ad9b0b26d1e
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2025-01-06 15:24:05 +0500

    up models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,6 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -14,8 +15,6 @@ class CarMake(models.Model):
 
 # CarModel model
 class CarModel(models.Model):
-
-
     car_make = models.ForeignKey(
         CarMake, on_delete=models.CASCADE
         )  # Many-to-One relationship
